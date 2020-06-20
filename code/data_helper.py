bebf429a8f5568ffec0ccb96a6862fead46562e7
lend-me-ears <lend.me.ears@gmail.com>
2020-06-21 00:00:16 +0800

    branch dev init.

---
+++ code/data_helper.py
@@ -0,0 +1,59 @@
+import json
+from email import utils
+import time
+from datetime import datetime
+import os
+from mutagen.mp3 import MP3
+
+
+def get_timestamp(date):
+    """
+    得到 rfc2822 时间戳
+    :param date: 日期 格式：20200222
+    :return: 时间戳
+    """
+    date_time = datetime.strptime(date, '%Y%m%d').replace(hour=18)
+    date_tuple = date_time.timetuple()
+    date_timestamp = time.mktime(date_tuple)
+    res = utils.formatdate(date_timestamp)
+    return res
+
+
+def get_information(path, year):
+    """
+    爬取文件列表数据
+    :param path: 文件夹
+    :param year: 年份
+    :return: None
+    """
+    res = []
+    # base_url = 'https://one.lend-me-ears.workers.dev/?file='
+    base_url = 'https://drive.lend-me-ears.workers.dev'
+    filelist = os.listdir(path)
+    for file in filelist:
+        if file.find('.mp3') < 0:  # 跳过非音频文件
+            continue
+        filepath = os.path.join(path, file)
+        size = os.path.getsize(filepath)
+        url = base_url + '/' + year + '/' + file
+        date = file.split(' ')[0]
+        timestamp = get_timestamp(date)
+        name = file.rsplit('.', 1)[0]
+        guid = 'a' + str(size) + str(date)
+        duration = int(MP3(filepath).info.length)
+        res.append({
+            'name': name,
+            'url': url,
+            'duration': duration,
+            'size': size,
+            'date': date,
+            'timestamp': timestamp,
+            'guid': guid
+        })
+
+    with open('../data/data_' + year + '.json', 'w') as f:
+        f.write(json.dumps(res, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    get_information('/Users/zhangxiaoyu/Downloads/耳朵借我/2020', '2020')
---
+++ code/rss_generator.py
@@ -0,0 +1,75 @@
+import xml.etree.ElementTree as ET
+import json
+
+year_list = ['17','20']
+
+
+def rss_generator():
+    rss_temp_file = '../rss/rss_template.xml'
+    rss_file = '../rss/rss.xml'
+    tree = ET.parse(rss_temp_file)
+    rss = tree.getroot()
+    channel = rss[0]
+    for year in year_list:
+        items = single_data_reader(year)
+        for item in items:
+            channel.append(item)
+    tree.write(rss_file, 'UTF-8')
+
+
+def single_data_reader(year):
+    json_file = '../data/data_20' + year + '.json'
+    item_list = []
+    with open(json_file) as f:
+        ep_list = json.loads(f.read())
+        for ep in ep_list:
+            info = {
+                'image': 'https://raw.githubusercontent.com/lend-me-ears/lend-me-ears/master/avatar.png',
+                'description': ep['name'] + '    ' + ep['date'],
+                'link': 'https://github.com/lend-me-ears/lend-me-ears',
+                'length': str(ep['size']),
+                'file': ep['url'],
+                'title': ep['name'],
+                'guid': ep['guid'],
+                'date': ep['timestamp'],
+                'duration':str(ep['duration'])
+            }
+            item = getItem(info)
+            item_list.append(item)
+    return item_list
+
+
+def getItem(info):
+    """
+    添加单集信息
+    :param info: 单集信息
+    :return: Element
+    """
+    item = ET.Element('item')
+
+    episodeType = ET.SubElement(item, 'itunes:episodeType')
+    episodeType.text = 'full'
+    explicit = ET.SubElement(item, 'itunes:explicit')
+    explicit.text = 'false'
+
+    image = ET.SubElement(item, 'itunes:image', {'href': info['image']})
+    enclosure = ET.SubElement(item, 'enclosure', {'length': info['length'], 'type': 'audio/mpeg', 'url': info['file']})
+
+    title = ET.SubElement(item, 'title')
+    title.text = info['title']
+    description = ET.SubElement(item, 'description')
+    description.text = info['description']
+    link = ET.SubElement(item, 'link')
+    link.text = info['link']
+    guid = ET.SubElement(item, 'guid')
+    guid.text = info['guid']
+    pubDate = ET.SubElement(item, 'pubDate')
+    pubDate.text = info['date']
+    duration = ET.SubElement(item, 'itunes:duration')
+    duration.text = info['duration']
+
+    return item
+
+
+if __name__ == '__main__':
+    rss_generator()
